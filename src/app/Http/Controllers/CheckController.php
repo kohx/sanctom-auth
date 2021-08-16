17aabe3e00ba766488f4710927b0feeb57da0e6b
okuda kohei <okuda@aska-ltd.jp>
2021-08-16 17:52:25 +0900

    creating storage

--- src/app/Http/Controllers/CheckController.php
+++ src/app/Http/Controllers/CheckController.php
@@ -3,27 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Http\File;
 use App\Models\Post;
 
 class CheckController extends Controller
 {
     public function index(Request $request)
     {
-        dump(Post::all()->toArray());
-    }
+        ## ファイルのURL
+        $disk = Storage::disk('public');
+        $url = $disk->url('test.txt');
+        dump($url);
 
-    public function show(Request $request, Post $post)
-    {
-        dump($post->toArray());
-    }
+        /**
+         * diskインスタンス取得
+         */
+        $disk = Storage::disk('public');
+        dump($disk);
 
-    public function store(Request $request, Post $post)
-    {
-        dump($post->toArray());
-    }
+        /**
+         * ファイルのコンテンツを取得
+         */
+        $file = $disk->get('test.txt');
+        dump($file);
 
-    public function destroy(Request $request, Post $post)
-    {
-        dump($post->toArray());
+        // /**
+        //  * ファイルを作成して「'saved!'」内容で保存
+        //  */
+        // $disk->put('storage/saved.txt', 'saved!');
+
+        // /**
+        //  * 指定したPATHにファイルを保存
+        //  */
+
+        //  // ストレージに置いたsample/sample.txtをファイルオブジェクトとして取得
+        // $file_path = storage_path('sample/sample.txt');
+        // $tmpFile = new File($file_path);
+
+        // $file = $disk->putFile(`storage`, $tmpFile);
     }
 }
