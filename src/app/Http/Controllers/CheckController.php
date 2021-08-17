b99fc1a105f397ff588f4888565b7f5a76a9ad81
kohei okuda <kohei.0728@gmail.com>
2021-08-17 18:13:58 +0900

    creating s3

--- src/app/Http/Controllers/CheckController.php
+++ src/app/Http/Controllers/CheckController.php
@@ -16,32 +16,5 @@ class CheckController extends Controller
         $disk = Storage::disk('public');
         $url = $disk->url('test.txt');
         dump($url);
-
-        /**
-         * diskインスタンス取得
-         */
-        $disk = Storage::disk('public');
-        dump($disk);
-
-        /**
-         * ファイルのコンテンツを取得
-         */
-        $file = $disk->get('test.txt');
-        dump($file);
-
-        // /**
-        //  * ファイルを作成して「'saved!'」内容で保存
-        //  */
-        // $disk->put('storage/saved.txt', 'saved!');
-
-        // /**
-        //  * 指定したPATHにファイルを保存
-        //  */
-
-        //  // ストレージに置いたsample/sample.txtをファイルオブジェクトとして取得
-        // $file_path = storage_path('sample/sample.txt');
-        // $tmpFile = new File($file_path);
-
-        // $file = $disk->putFile(`storage`, $tmpFile);
     }
 }
